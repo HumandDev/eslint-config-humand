08cf71b335cb1577779c0ae86903ca6f7e591161
mellamopepino <delfinagarciav@gmail.com>
2024-01-18 12:03:20 -0300

    [FIX] Specify tsconfigRootDir

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     "project": "./tsconfig.json",
-    "tsconfigRootDir": "__dirname",
+    "tsconfigRootDir": __dirname,
   },
   extends: [
     "airbnb-typescript",
