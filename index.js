c6ae8cc6059e7fcceaf7f1ccc71b74839ce1bd21
mellamopepino <delfinagarciav@gmail.com>
2024-01-22 17:17:05 -0300

    [FIX] Cambiar config

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// eslint-plugin-humand
+// eslint-config-humand
 
 module.exports = {
   env: {
@@ -36,12 +36,22 @@ module.exports = {
     "no-return-assign": "off",
     "no-restricted-properties": "off",
     "no-underscore-dangle": "off",
-    "no-extra-semi": "off",
+    "no-extra-semi": "error",
     "no-mixed-spaces-and-tabs": "off",
+    "no-nested-ternary": "error",
+    "no-console": [
+      "error",
+      { allow: ["error"] },
+    ],
+    "no-extra-boolean-cast": [
+      "error",
+      {"enforceForLogicalOperands": true},
+    ],
     "consistent-return": "off",
     "comma-spacing": "off",
     "comma-dangle": "off",
     "radix": "off",
+    "default-param-last": "off",
 
     "import/prefer-default-export": "off",
     "import/no-named-default": "off",
@@ -66,6 +76,8 @@ module.exports = {
       "always-multiline"
     ],
     "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/default-param-last": "error",
+    "@typescript-eslint/no-shadow": "error",
 
     "react/forbid-prop-types": "off",
     "react/jsx-filename-extension": "off",
@@ -74,63 +86,11 @@ module.exports = {
     "react/require-default-props": "off",
     "react/prop-types": "off",
     "react/no-array-index-key": "off",
+    "react/hook-use-state": "error",
+    "react/destructuring-assignment": "error",
 
     "jsx-a11y/media-has-caption": "off",
     "jsx-a11y/anchor-is-valid": "off",
-
-    // PROP: prender
-    // "no-nested-ternary": "error",
-    //
-    // Evita ternarios nesteados que dificultan la lectura
-    // "no-nested-ternary": "off",
-     "no-nested-ternary": "error",
-
-    // PROP: prender
-    // "default-param-last": "off",
-    // "@typescript-eslint/default-param-last": "error",
-    //
-    // Requiere que los parametros opcionales estén al final para que se puedan obviar al llamar a
-    // la función
-    // "@typescript-eslint/default-param-last": "off",
-    "default-param-last": "off",
-    "@typescript-eslint/default-param-last": "error",
-
-    // PROP: prender
-    // "no-console": ["error", { allow: ["error"] }],
-    //
-    // No permitir el uso de console, pero sí permitir console.error
-    // "no-console": "off",
-    "no-console": ["error", { allow: ["error"] }],
-
-    // PROP: prender:
-    // "react/hook-use-state": "error",
-    //
-    // Esta regla chequea que el useState siga el formato:
-    // const [thing, setThing] = useState();
-    "react/hook-use-state": "error",
-
-    // PROP: prender
-    // "react/destructuring-assignment": "error",
-    //
-    // Evita el uso de las props como `props.id` y requiere hacer `const { id } = props`
-    // "react/destructuring-assignment": "off",
-    "react/destructuring-assignment": "error",
-
-    // PROP 1: propongo dejarlo apagado, ya que tira muchos errores cuando se quieren reutilizar
-    // nombres. Como por ejemplo con nombres del tipo "error", "err", "item", "response", etc...
-    //
-    // PROP 2: a lo sumo consideraría prenderlo con una lista de nombres permitidos haciendo:
-    // "@typescript-eslint/no-shadow": ["error", { allow: ["error", "err", "item", "event",
-    // "response"] }],
-    // Aunque parece medio molesto tener que estar definiendo los nombres permitidos.
-    "@typescript-eslint/no-shadow": "off",
-
-    // PROP: prender:
-    // "no-extra-boolean-cast": ["error", {"enforceForLogicalOperands": true}],
-    //
-    // Evita casteos a booleanos innecesarios.
-    // "no-extra-boolean-cast": "off",
-    "no-extra-boolean-cast": ["error", {"enforceForLogicalOperands": true}],
 
     // DEPRECADO
     "object-curly-newline": "off",
